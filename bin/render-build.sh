d829710d2f9c22bb27bc080174abc8e2b30c4573
Maxie <maxie@maxie.bot>
2026-01-31 02:34:30 +0000

    Add Solid Cable messages table to main database
    - Create solid_cable_messages table in main db (single-db setup)
    - Simplify render-build.sh to just run db:migrate
    - Fixes: relation 'solid_cable_messages' does not exist error

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,3 @@ set -o errexit
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails db:migrate
-bundle exec rails db:migrate:cable
-bundle exec rails db:migrate:cache
-bundle exec rails db:migrate:queue
