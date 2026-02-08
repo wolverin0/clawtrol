cd4a29839df7bf2d0704217e8bb2eebaad5454d3
wolverin0 <ggorbalan@gmail.com>
2026-02-08 12:56:00 -0300

    Show task card badges without hover

---
+++ test/views/boards/task_card_badges_visibility_test.rb
@@ -0,0 +1,24 @@
+require "test_helper"
+
+class Boards::TaskCardBadgesVisibilityTest < ActiveSupport::TestCase
+  test "task card badges are not hidden behind hover-only opacity classes" do
+    task = Task.create!(
+      name: "Badge visibility test",
+      user: users(:one),
+      board: boards(:one),
+      status: :inbox,
+      priority: :none,
+      model: "opus",
+      nightly: true
+    )
+
+    html = ApplicationController.render(
+      partial: "boards/task_card",
+      locals: { task: task }
+    )
+
+    assert_includes html, "OPUS"
+    assert_includes html, "nightly-badge"
+    assert_not_includes html, "opacity-0 group-hover:opacity-100"
+  end
+end
