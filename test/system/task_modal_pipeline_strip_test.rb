d29bbaf3c4902e895e628e03d5854691cfb84343
wolverin0 <ggorbalan@gmail.com>
2026-02-15 14:36:27 -0300

    fix(pipeline/mobile): dashboard + modal scroll/context menu (#691)

---
+++ test/system/task_modal_pipeline_strip_test.rb
@@ -0,0 +1,25 @@
+require "application_system_test_case"
+
+class TaskModalPipelineStripTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:one)
+    @board = boards(:one)
+    @task = tasks(:one)
+
+    @task.update!(user: @user, board: @board, pipeline_enabled: true, pipeline_stage: "triaged")
+
+    sign_in_as(@user)
+  end
+
+  test "task modal renders a horizontally-scrollable pipeline strip container" do
+    skip "Requires JavaScript support" unless ApplicationSystemTestCase::CHROME_AVAILABLE
+
+    visit board_path(@board)
+    assert_selector "h2", text: "Inbox", wait: 5
+
+    find("#task_#{@task.id} a[data-turbo-frame='task_panel']").click
+    assert_selector "turbo-frame#task_panel [data-controller*='task-modal']", wait: 10
+
+    assert_selector "turbo-frame#task_panel [data-testid='pipeline-strip']", wait: 10
+  end
+end
