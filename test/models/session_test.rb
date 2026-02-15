373ec552ee4c4df153046d203eb2f850f945e487
MiniMax Factory <minimax@otacon.local>
2026-02-15 15:17:57 -0300

    [factory] model: add scopes and validations to Session model

--- test/models/session_test.rb
+++ test/models/session_test.rb
@@ -7,6 +7,7 @@ class SessionTest < ActiveSupport::TestCase
     @user = User.first || User.create!(email_address: "test@test.com", password: "password123456")
   end
 
+  # === Basic validations ===
   test "valid with user" do
     session = Session.new(user: @user)
     assert session.valid?
@@ -15,10 +16,39 @@ class SessionTest < ActiveSupport::TestCase
   test "requires user" do
     session = Session.new(user: nil)
     assert_not session.valid?
+    assert_includes session.errors[:user], "must exist"
   end
 
   test "belongs_to user" do
     session = Session.create!(user: @user)
     assert_equal @user, session.user
+  end
+
+  # === Scopes ===
+  test "scope for_user scopes by user" do
+    session1 = Session.create!(user: @user)
+    user2 = User.create!(email_address: "other@test.com", password: "password123456")
+    session2 = Session.create!(user: user2)
+
+    assert_includes Session.for_user(@user), session1
+    assert_not_includes Session.for_user(@user), session2
+  end
+
+  test "scope recent orders by created_at" do
+    session_old = Session.create!(user: @user)
+    session_new = Session.create!(user: @user)
+
+    assert_equal [session_new.id, session_old.id], Session.recent.ids
+  end
+
+  # === Validations ===
+  test "ip_address accepts valid length" do
+    session = Session.new(user: @user, ip_address: "192.168.1.1")
+    assert session.valid?
+  end
+
+  test "user_agent accepts valid length" do
+    session = Session.new(user: @user, user_agent: "Mozilla/5.0 (Test)")
+    assert session.valid?
   end
 end
