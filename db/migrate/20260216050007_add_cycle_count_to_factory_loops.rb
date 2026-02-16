5c4924dead67a14844225a49cf9eeecbd4531d50
wolverin0 <ggorbalan@gmail.com>
2026-02-16 02:28:38 -0300

    fix: make remaining imported column migrations idempotent

--- db/migrate/20260216050007_add_cycle_count_to_factory_loops.rb
+++ db/migrate/20260216050007_add_cycle_count_to_factory_loops.rb
@@ -1,5 +1,7 @@
 class AddCycleCountToFactoryLoops < ActiveRecord::Migration[8.1]
   def change
+    return if column_exists?(:factory_loops, :cycle_count)
+
     add_column :factory_loops, :cycle_count, :integer, default: 0
   end
 end
