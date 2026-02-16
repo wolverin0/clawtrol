d6c4da7c122a8a68716c606a416d883fbd1d5c70
wolverin0 <ggorbalan@gmail.com>
2026-02-16 11:50:10 -0300

    fix: make missing user_id index migration idempotent

--- db/migrate/20260215235000_add_missing_user_id_indexes.rb
+++ db/migrate/20260215235000_add_missing_user_id_indexes.rb
@@ -4,13 +4,17 @@ class AddMissingUserIdIndexes < ActiveRecord::Migration[8.0]
   disable_ddl_transaction!
 
   def change
-    # agent_transcripts.user_id - frequently filtered by user, no index exists
-    add_index :agent_transcripts, :user_id, algorithm: :concurrently, if_not_exists: true
+    add_index_if_column_exists(:agent_transcripts, :user_id)
+    add_index_if_column_exists(:task_activities, :user_id)
+    add_index_if_column_exists(:webhook_logs, :user_id)
+  end
 
-    # task_activities.user_id - user activity queries
-    add_index :task_activities, :user_id, algorithm: :concurrently, if_not_exists: true
+  private
 
-    # webhook_logs.user_id - user webhook history queries
-    add_index :webhook_logs, :user_id, algorithm: :concurrently, if_not_exists: true
+  def add_index_if_column_exists(table_name, column_name)
+    return unless table_exists?(table_name)
+    return unless column_exists?(table_name, column_name)
+
+    add_index table_name, column_name, algorithm: :concurrently, if_not_exists: true
   end
 end
