107831df9e086a319f9e2e75bf51c5a39e40e01a
wolverin0 <ggorbalan@gmail.com>
2026-02-16 02:28:54 -0300

    fix: make duplicate tasks_count migration idempotent

--- db/migrate/20260216050009_add_tasks_count_to_agent_personas.rb
+++ db/migrate/20260216050009_add_tasks_count_to_agent_personas.rb
@@ -1,5 +1,7 @@
 class AddTasksCountToAgentPersonas < ActiveRecord::Migration[8.1]
   def change
+    return if column_exists?(:agent_personas, :tasks_count)
+
     add_column :agent_personas, :tasks_count, :integer, default: 0
   end
 end
