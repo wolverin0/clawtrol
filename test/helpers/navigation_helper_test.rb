6ed088e204c5f2edc0ce805feb57c99dcb749a1a
wolverin0 <ggorbalan@gmail.com>
2026-02-24 20:54:20 -0300

    test: [factory] add primary_navigation board-link regression tests

--- test/helpers/navigation_helper_test.rb
+++ test/helpers/navigation_helper_test.rb
@@ -10,13 +10,31 @@ class NavigationHelperTest < ActiveSupport::TestCase
       @session = {}
     end
 
-    def boards_path
-      "/boards"
-    end
-
+    def boards_path = "/boards"
     def board_path(board)
-      "/boards/#{board.id}"
+      board_id = board.respond_to?(:id) ? board.id : board
+      "/boards/#{board_id}"
     end
+    def workflows_path = "/workflows"
+    def factory_path = "/factory"
+    def nightshift_path = "/nightshift"
+    def sessions_explorer_path = "/sessions"
+    def nodes_path = "/nodes"
+    def skill_manager_path = "/skills"
+    def agent_personas_path = "/agent_personas"
+    def roster_agent_personas_path = "/agent_personas/roster"
+    def agent_config_path = "/agent_config"
+    def analytics_path = "/analytics"
+    def tokens_path = "/tokens"
+    def outputs_path = "/outputs"
+    def dm_scope_audit_path = "/self_audit"
+    def showcases_path = "/showcases"
+    def command_path = "/command"
+    def terminal_path = "/terminal"
+    def saved_links_path = "/saved_links"
+    def learning_proposals_path = "/learning_proposals"
+    def webhook_mappings_path = "/webhook_mappings"
+    def gateway_config_path = "/gateway_config"
   end
 
   test "last_board_navigation_path falls back to boards path without session board id" do
@@ -43,5 +61,29 @@ class NavigationHelperTest < ActiveSupport::TestCase
     host.session[:last_board_id] = boards(:two).id
 
     assert_equal "/boards", host.last_board_navigation_path
+  end
+
+  test "primary_navigation board item uses last board navigation path" do
+    host = NavigationHelperHost.new
+    host.current_user = users(:one)
+    host.session[:last_board_id] = boards(:one).id
+
+    board_item = host.primary_navigation
+      .find { |category| category[:id] == :tasks }[:items]
+      .find { |item| item[:label] == "Board" }
+
+    assert_equal "/boards/#{boards(:one).id}", board_item[:url]
+  end
+
+  test "primary_navigation board item falls back to boards path for unowned board" do
+    host = NavigationHelperHost.new
+    host.current_user = users(:one)
+    host.session[:last_board_id] = boards(:two).id
+
+    board_item = host.primary_navigation
+      .find { |category| category[:id] == :tasks }[:items]
+      .find { |item| item[:label] == "Board" }
+
+    assert_equal "/boards", board_item[:url]
   end
 end
