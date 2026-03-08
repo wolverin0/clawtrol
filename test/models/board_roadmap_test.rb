84a8e36423a6090814a3a9d03d7fa3dd872380a6
wolverin0 <ggorbalan@gmail.com>
2026-03-08 20:09:44 -0300

    test: add unit tests for BoardRoadmap checked_items and progress_summary (#692)

--- test/models/board_roadmap_test.rb
+++ test/models/board_roadmap_test.rb
@@ -22,6 +22,36 @@ class BoardRoadmapTest < ActiveSupport::TestCase
     ], roadmap.unchecked_items.map { |item| item[:text] }
   end
 
+  test "checked_items parses completed checkboxes" do
+    roadmap = BoardRoadmap.new(
+      board: boards(:one),
+      body: <<~MD
+        - [x] Done item one
+        - [X] Done item two
+        - [ ] Not done yet
+        * [x] Also done
+      MD
+    )
+
+    items = roadmap.checked_items
+    assert_equal 3, items.size
+    assert_equal ["Done item one", "Done item two", "Also done"], items.map { |i| i[:text] }
+  end
+
+  test "progress_summary returns X/Y format" do
+    roadmap = BoardRoadmap.new(
+      board: boards(:one),
+      body: "- [x] Done\n- [ ] Pending\n- [x] Also done\n- [ ] Another pending"
+    )
+
+    assert_equal "2/4", roadmap.progress_summary
+  end
+
+  test "progress_summary returns nil when no checklist items" do
+    roadmap = BoardRoadmap.new(board: boards(:one), body: "Just some notes")
+    assert_nil roadmap.progress_summary
+  end
+
   test "unchecked_items ignores checked items and deduplicates by normalized text" do
     roadmap = BoardRoadmap.new(
       board: boards(:one),
